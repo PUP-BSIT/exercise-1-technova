c4120b355185680b8316d1c7048c3a262a9fa72f
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-09-15 14:54:08 +0800

    setup watch ts --watch ./input --outDir ./output

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Department;
+(function (Department) {
+    Department["HR"] = "Human Resources";
+    Department["IT"] = "Information Technology";
+    Department["SALES"] = "Sales";
+    Department["MARKETING"] = "Marketing";
+    Department["FINANCE"] = "Finance";
+})(Department || (Department = {}));
+// TODO (Andaya, Gener)
+// TODO (Barcelos, Kevin)
+// TODO (Gomez, Hermione)
+// TODO (Relente, Patricia Joy)
+// TODO (Uy, Angelica Joy)
+//# sourceMappingURL=script.js.map
